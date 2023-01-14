7edb0fc4c884245f53447b3ad28cb0aa75aa292a
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2023-01-14 19:35:37 -0300

    Implementado aula de conceituação de POO

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        System.out.println(curso1);
+    }
+}
